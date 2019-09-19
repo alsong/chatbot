78ccb6474beaa3260956934485d1cd40e96e5afc
alsong <alsongdunstan2@gmail.com>
2019-09-19 13:47:31 +0300

    Added more changes

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     app = express().use(bodyParser.json()); //creates an http server
 
+    const request = require('request');
+
 //sets up server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+//page access token
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 //creates the endpoint for our webhook
 app.post('/webhook', (req,res) =>{
@@ -24,6 +29,18 @@ app.post('/webhook', (req,res) =>{
 
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+
+            //Get the sender ID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            //Check if the event is a a message or a postback and
+            //pass the event to the appropriate handler function
+            if(webhook_event.message){
+                handleMessage(sender_psid,webhook_event.message);
+            }else if(webhook_event.postback){
+                handlePostback(sender_psid, webhook_event.postback);
+            }
         })
 
         //returns a '200 OK' response to all requests
@@ -62,3 +79,53 @@ app.get('/webhook', (req,res) =>{
         }
     }
 })
+
+//Handles messages events
+const handleMessage = (sender_psid, received_message) =>{
+
+    let response;
+
+    //Check if the message contains text
+    if(received_message.text){
+
+        //Create the payload for a basic text message
+        response = {
+            "text" : `You sent the message: '${received_message.text}'. Now send me an image! `
+        }
+    }
+
+    //Sends the response message
+    callSendAPI(sender_psid,response);
+}
+
+//Handles messaging_postbacks events
+const handlePostback = (sender_psid, received_postback) =>{
+
+}
+
+//Sends response messages via the Send API
+const callSendAPI = (sender_psid, response) =>{
+
+    //Construct the message body
+    let request_body = {
+        "recipient" : {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    //Send the HTTP request to the Messenger Platfrom
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs" : { 
+            "access_token" : process.env.PAGE_ACCESS_TOKEN
+        },
+        "method": request_body
+    },(err,res,body) =>{
+        if(!err){
+            console.log('message sent')
+        }else{
+            console.error('Unable to send message: ' + err);
+        }
+    })
+}

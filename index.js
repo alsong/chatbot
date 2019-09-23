2c2730d37cf7284c242fa4a36518ed8bd35e8d15
Alsong <alsongdunstan2@gmail.com>
2019-09-23 14:58:11 +0300

    more changes

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
+const FACEBOOK_GRAPH_API_BASE_URL = 'https://graph.facebook.com/v2.6/';
+const GREETING = 'GREETING';
+const START_SEARCH_NO = 'START_SEARCH_NO';
+const START_SEARCH_YES = 'START_SEARCH_YES';
+const VIEW_OPTIONS = 'VIEW_OPTIONS';
+const VIEW_SERVICES = 'VIEW_SERVICES';
 
 // Imports dependencies and set up http server
 const
@@ -26,22 +32,29 @@ app.post('/webhook', (req, res) => {
         // Iterates over each entry - there may be multiple if batched
         body.entry.forEach(function (entry) {
 
-            // Gets the message. entry.messaging is an array, but 
-            // will only ever contain one message, so we get index 0
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
+            // Iterate over each messaging event and handle accordingly
+            entry.messaging.forEach((messagingEvent) => {
+                console.log({
+                    messagingEvent
+                });
 
-            // Get the sender PSID
-            let sender_psid = webhook_event.sender.id;
-            console.log('Sender PSID: ' + sender_psid);
-
-            // Check if the event is a message or postback and
-            // pass the event to the appropriate handler function
-            if (webhook_event.message) {
-                handleMessage(sender_psid, webhook_event.message);
-            } else if (webhook_event.postback) {
-                handlePostback(sender_psid, webhook_event.postback);
-            }
+                // Check if the event is a message or postback and
+                // pass the event to the appropriate handler function
+                if (messagingEvent.postback) {
+                    handlePostback(messagingEvent.sender.id, messagingEvent.postback);
+                } else if (messagingEvent.message) {
+                    if (messagingEvent.message.quick_reply) {
+                        handlePostback(messagingEvent.sender.id, messagingEvent.message.quick_reply);
+                    } else {
+                        handleMessage(messagingEvent.sender.id, messagingEvent.message);
+                    }
+                } else {
+                    console.log(
+                        'Webhook received unknown messagingEvent: ',
+                        messagingEvent
+                    );
+                }
+            })
         });
 
         // Returns a '200 OK' response to all requests
@@ -57,7 +70,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+    let VERIFY_TOKEN = "12345"
 
     // Parse the query params
     let mode = req.query['hub.mode'];
@@ -127,24 +140,35 @@ function handleMessage(sender_psid, received_message) {
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
-    let response;
-  
-  // Get the payload for the postback
-  let payload = received_postback.payload;
+    // Get the payload for the postback
+    let payload = received_postback.payload;
 
-  // Set the response based on the postback payload
-  if (payload === 'yes') {
-    response = { "text": "Thanks!" }
-  } else if (payload === 'no') {
-    response = { "text": "Oops, try sending another image." }
-  }
-  // Send the message to acknowledge the postback
-  callSendAPI(sender_psid, response);
+    // Set the response based on the postback payload
+    switch (payload) {
+        case GREETING:
+            handleGreetingPostback(sender_psid);
+            break;
+        case START_SEARCH_YES:
+            handleStartYesPostback(sender_psid);
+            break;
+        case START_SEARCH_NO:
+            handleStartNoPostback(sender_psid);
+            break;
+        case VIEW_OPTIONS:
+            handleViewOptions(sender_psid);
+            break;
+        case VIEW_SERVICES:
+            handleViewServices(sender_psid);
+            break;
+        default:
+            console.log('Cannot differentiate the payload type');
+    }
 }
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
     // Construct the message body
+    console.log('message to be sent: ', response);
     let request_body = {
         "recipient": {
             "id": sender_psid
@@ -154,7 +178,7 @@ function callSendAPI(sender_psid, response) {
 
     // Send the HTTP request to the Messenger Platform
     request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "uri": `${FACEBOOK_GRAPH_API_BASE_URL}me/messages`,
         "qs": {
             "access_token": PAGE_ACCESS_TOKEN
         },
@@ -167,4 +191,151 @@ function callSendAPI(sender_psid, response) {
             console.error("Unable to send message:" + err);
         }
     });
+}
+
+//Handles Greeting postback 
+function handleGreetingPostback(sender_psid) {
+    request({
+        url: `${FACEBOOK_GRAPH_API_BASE_URL}${sender_psid}`,
+        qs: {
+            access_token: PAGE_ACCESS_TOKEN,
+            fields: "first_name"
+        },
+        method: "GET"
+    }, function (error, response, body) {
+        var greeting = "";
+        if (error) {
+            console.log("Error getting user's name: " + error);
+        } else {
+            var bodyObj = JSON.parse(body);
+            const name = bodyObj.first_name;
+            greeting = "Hi " + name + ". ";
+        }
+        const message = greeting + "Would you like to make an order in your area?";
+        const greetingPayload = {
+            "text": message,
+            "quick_replies": [{
+                    "content_type": "text",
+                    "title": "Yes!",
+                    "payload": START_SEARCH_YES
+                },
+                {
+                    "content_type": "text",
+                    "title": "No, thanks.",
+                    "payload": START_SEARCH_NO
+                }
+            ]
+        };
+        callSendAPI(sender_psid, greetingPayload);
+    });
+}
+
+//Handles Start No postback
+function handleStartNoPostback(sender_psid) {
+    const noPayload = {
+        "text": "That's ok my friend, when you want to make an order try out our menu"
+    };
+    callSendAPI(sender_psid, noPayload);
+}
+
+//Handles Start Yes postback
+function handleStartYesPostback(sender_psid) {
+    const campaigns = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                        "title": "We need your help",
+                        "image_url": "http://awsassets.panda.org/img/original/wwf_infographic_tropical_deforestation.jpg",
+                        "subtitle": "to save our natural world",
+                        "buttons": [{
+                            "type": "postback",
+                            "payload": VIEW_OPTIONS,
+                            "title": "Javan Rhino Appeal"
+                        }]
+                    },
+                    {
+                        "title": "We need your help",
+                        "image_url": "http://awsassets.panda.org/img/original/wwf_infographic_tropical_deforestation.jpg",
+                        "subtitle": "to save our natural world",
+                        "buttons": [{
+                            "type": "postback",
+                            "payload": VIEW_OPTIONS,
+                            "title": "Javan Rhino Appeal"
+                        }]
+                    }
+                ]
+            }
+        }
+    };
+    callSendAPI(sender_psid, campaigns);
+}
+
+//Handle View Options
+function handleViewOptions(sender_psid) {
+    const campaigns = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                        "title": "Panda ",
+                        "image_url": "http://awsassets.panda.org/img/original/wwf_infographic_tropical_deforestation.jpg",
+                        "subtitle": "to save our natural world",
+                        "buttons": [{
+                            "type": "postback",
+                            "payload": VIEW_SERVICES,
+                            "title": "Javan Rhino Appeal"
+                        }]
+                    },
+                    {
+                        "title": "Infograph",
+                        "image_url": "http://awsassets.panda.org/img/original/wwf_infographic_tropical_deforestation.jpg",
+                        "subtitle": "to save our natural world",
+                        "buttons": [{
+                            "type": "postback",
+                            "payload": VIEW_SERVICES,
+                            "title": "Javan Rhino Appeal"
+                        }]
+                    }
+                ]
+            }
+        }
+    };
+    callSendAPI(sender_psid, campaigns);
+}
+
+//Hnadle View Services
+function handleViewServices(sender_psid) {
+    const campaigns = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                        "title": "Panda ",
+                        "image_url": "http://awsassets.panda.org/img/original/wwf_infographic_tropical_deforestation.jpg",
+                        "subtitle": "to save our natural world",
+                        "buttons": [{
+                            "type": "postback",
+                            "payload": VIEW_SERVICES,
+                            "title": "Javan Rhino Appeal"
+                        }]
+                    },
+                    {
+                        "title": "Infograph",
+                        "image_url": "http://awsassets.panda.org/img/original/wwf_infographic_tropical_deforestation.jpg",
+                        "subtitle": "to save our natural world",
+                        "buttons": [{
+                            "type": "postback",
+                            "payload": VIEW_SERVICES,
+                            "title": "Javan Rhino Appeal"
+                        }]
+                    }
+                ]
+            }
+        }
+    };
+    callSendAPI(sender_psid, campaigns);
 }
